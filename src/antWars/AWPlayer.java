b2c8139682887bea379796a951ad58501f4df4c8
CergyCZ <cerhanjaroslav@gmail.com>
2022-02-16 08:57:28 +0100

    a

--- src/antWars/AWPlayer.java
+++ src/antWars/AWPlayer.java
@@ -7,6 +7,29 @@ public class AWPlayer {
 	private Deck defenders;
 	private Base base;
 	
+	public AWPlayer(int hp) {
+		deck = new Deck();
+		discardPile = new Deck();
+		defenders = new Deck();
+		base = new Base(hp);
+	}
+
+	public Deck getDeck() {
+		return deck;
+	}
+
+	public Deck getDiscardPile() {
+		return discardPile;
+	}
+
+	public Deck getDefenders() {
+		return defenders;
+	}
+
+	public Base getBase() {
+		return base;
+	}
+	
 	
 	
 	
--- src/antWars/Main.java
+++ src/antWars/Main.java
@@ -2,8 +2,16 @@ package antWars;
 
 public class Main {
 
+	private final int INITHP = 30;
+	
+	
+	AWPlayer player1 = new AWPlayer(INITHP);
+	AWPlayer player2 = new AWPlayer(INITHP);
+	
+	Deck centralDeck = new Deck();
+	
 	
 	public static void main(String[] args) {
-		//go
+		player1
 	}
 }
