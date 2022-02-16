5192e5bdc9d0ea4ed2ad215c345a2d5c844d20b8
CergyCZ <cerhanjaroslav@gmail.com>
2022-02-16 09:00:41 +0100

    ad

--- src/antWars/Main.java
+++ src/antWars/Main.java
@@ -2,16 +2,18 @@ package antWars;
 
 public class Main {
 
-	private final int INITHP = 30;
-	
-	
-	AWPlayer player1 = new AWPlayer(INITHP);
-	AWPlayer player2 = new AWPlayer(INITHP);
-	
-	Deck centralDeck = new Deck();
+
+
 	
 	
 	public static void main(String[] args) {
-		player1
+		
+		final int INITHP = 30;
+		
+		
+		AWPlayer player1 = new AWPlayer(INITHP);
+		AWPlayer player2 = new AWPlayer(INITHP);
+		
+		Deck centralDeck = new Deck();
 	}
 }
